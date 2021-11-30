d502dd2ceb0aa0f85fac4f62955c0eb32c2f8330
Lesli <Lesli@LAPTOP-G4BQ0J6E>
2021-11-30 09:33:28 -0600

    Se agrego el método Shellsort

--- src/datos/DatosDesordenados.java
+++ src/datos/DatosDesordenados.java
@@ -56,6 +56,39 @@ public class DatosDesordenados {
     }
     
     
+
+
+  private void Shellsort(int[]v){
+  final int N=v.length;
+    int incremento=N;
+      do{
+        incremento=incremento/2;
+         for(int k=0; k< incremento;k++){
+             for(int i=incremento +k; i<N; i+=incremento){
+                 int j=i;
+                 while(j-incremento>= 0 && v[j]<v[j-incremento]){
+                     int tmp=v[j];
+                     v[j]=v[j-incremento];
+                     v[j-incremento]=tmp;
+                     j-=incremento;
+                 }
+             }
+         }
+         
+      }while(incremento >1);
+  }
+     public DatosOrdenados ShellSort(){
+        
+        // obtener una copia de los datos desordenados
+        int [] x = this.getCopiaValores();
+        
+        // agregar algoritmo de ordenación
+        Shellsort(x);
+        
+        // devolver los datos ordenados
+        return new DatosOrdenados(x);
+    }
+
    
      public DatosOrdenados algoritmoSort(){
         
