4cde89c665e761198f9fce51506367d191d72fa6
Lesli <Lesli@LAPTOP-G4BQ0J6E>
2021-11-30 16:24:52 -0600

    Se agrego la clase DatosDesordenados modificada, añadiendo el metodo ShellSort de manera Ascendente y Descendente

--- src/datos/DatosDesordenados.java
+++ src/datos/DatosDesordenados.java
@@ -1,229 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package datos;
 
 /**
  *
- * @author ALZTeam
+ * @author Lesli
  */
 public class DatosDesordenados {
-    // Atributos
-    private int[] valores; 
-    private int limiteSuperior; 
-    private int size; 
 
-    public DatosDesordenados(int limite, int size) {
-        this.valores = new int[size];
-        this.limiteSuperior = limite;
-        this.size = size;
-    }
-    
-    public void agregarDato(int valor){
-        valores[size]= valor;
-        size++;
-    }
-    
-    public int getDato(int posicion){
-        return valores[posicion];
-    }
-    
-    public void generarValores(){
-        for (int i=0; i < valores.length; i++){
-            double aleatorio = Math.random() * limiteSuperior;
-            valores[i] = (int) aleatorio + 1;
-        }
-    }
-    
-    public int getSize(){
-        return size; 
-    }
-    
-    public int getCapacidad(){
-        return valores.length;
-    }
-    
-    public int getLimiteSuperior(){
-        return limiteSuperior;
-    }
-    
-    
-    public int[] getValores(){
-        return valores;
-    } 
-    
-    public int[] getCopiaValores(){
-        int [] copia = new int[size];
-        System.arraycopy(valores, 0, copia, 0, size);
-        return copia;
-    }
-    
-    
+    int i, j, temp, pas;
 
-
-  private void Shellsort(int[]v){
-  final int N=v.length;
-    int incremento=N;
-      do{
-        incremento=incremento/2;
-         for(int k=0; k< incremento;k++){
-             for(int i=incremento +k; i<N; i+=incremento){
-                 int j=i;
-                 while(j-incremento>= 0 && v[j]<v[j-incremento]){
-                     int tmp=v[j];
-                     v[j]=v[j-incremento];
-                     v[j-incremento]=tmp;
-                     j-=incremento;
-                 }
-             }
-         }
-         
-      }while(incremento >1);
-  }
-     public DatosOrdenados ShellSort(){
-        
-        // obtener una copia de los datos desordenados
-        int [] x = this.getCopiaValores();
-        
-        // agregar algoritmo de ordenación
-        Shellsort(x);
-        
-        // devolver los datos ordenados
-        return new DatosOrdenados(x);
+    public DatosDesordenados() {
+        this.i = 0;
+        this.j = 0;
+        this.temp = 0;
     }
-
-   
-     public DatosOrdenados algoritmoSort(){
-        
-        // obtener una copia de los datos desordenados
-        int [] arreglo = this.getCopiaValores();
-        
-        // agregar algoritmo de ordenación
-        
-        // devolver los datos ordenados
-        return new DatosOrdenados(arreglo);
-    }
-    
-    public DatosOrdenados bubbleSort(){
-        int [] arreglo = this.getCopiaValores();
-        int n = arreglo.length;
-        int cont = 0;
-        int ci = 0;
-        for (int i=0; i < n-1; i++){
-            for (int j=0; j < n-i-1; j++){
-                cont++;
-                if (arreglo[j] > arreglo[j+1]){
-                    // intercambiar: swap
-                    ci++;
-                    int temporal = arreglo[j];
-                    arreglo[j] = arreglo[j+1];
-                    arreglo[j+1] = temporal;
+    //Metodo burbujaAscendente
+    public String burbujaSortAscendente(int[] arreglo) {
+        pas = 0;
+        for (i = 0; i < arreglo.length; i++) {
+            for (j = i + 1; j < arreglo.length; j++) {
+                if (arreglo[i] > arreglo[j]) {
+                    temp = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = temp;
                 }
+                pas++;
             }
         }
-        System.out.println("BS-comparaciones = " + cont);
-        System.out.println("BS-intercambios = " + ci);
-        return new DatosOrdenados(arreglo);
+        return mostrarArreglo(arreglo);
     }
     
-       
-    
-    public DatosOrdenados insertSort(){
-         // obtener una copia de los datos desordenados
-        int [] a = this.getCopiaValores();
-        
-	int i, j, aux; 
-	int cont=0; 
-	for ( i = 1; i < a.length; i++){
-		j = i;
-		aux = a[i];
-		while (j >0 && aux < a[j-1]){
-                   cont++;
-                   a[j] = a[j-1];
-                   j--; 
-		}
-		a[j] = aux; 
-	}
-        System.out.println("IS-comparaciones = " + cont);
-	// devolver los datos ordenados
-        return new DatosOrdenados(a);
-    }
-    
-    public DatosOrdenados mergeSort(){
-    int [] x = this.getCopiaValores();
-	// Aquí va la llamada al método mergesort( arreglo, l, r)
-	this.mergesort( x, 0, x.length - 1);
-        System.out.println("MS-comparaciones = " + cm);
-	return new DatosOrdenados(x);
-    } // fin del método ordenacionMerge
-    
-   private void mergesort (int v[], int l, int r) {
-		 int m = (r+l)/2; 
-		 if (r > l) { 
-                     cm++;
-			 mergesort (v, l, m);
-			 mergesort (v, m+1, r); 
-			 merge (v, l, m, r); 
-		 }
-	}
-    static int cm=0;
-   private void merge (int v[], int l, int m, int r)
-	{
-	//Encuentra el tamaño de los sub-vectores para unirlos.
-	  int n1 = m - l + 1;
-	  int n2 = r - m;
-
-	  //Vectores temporales.
-	  int arrIzq[] = new int [n1];
-	  int arrDer[] = new int [n2];
-
-	  //Copia los datos a los arreglos temporales.
-	  for (int i=0; i < n1; i++) {
-              cm++;
-		arrIzq[i] = v[l+i];
-	  }
-	  for (int j=0; j < n2; j++) {
-              cm++;
-		arrDer[j] = v[m + j + 1];
-	  }
-	  // Combinación (mezcla) de los dos arreglos:
-	  int i = 0, j = 0;
-	  int k = l;
-	  while (i < n1 && j < n2) {//Ordenamiento.
-                cm++;
-		if (arrIzq[i] <= arrDer[j]) {
-			v[k] = arrIzq[i];
-			i++;
-		} else {
-			v[k] = arrDer[j];
-			j++;
-		}
-		k++;
-	  }//Fin del while.
-
-	  /* Si quedan elementos por ordenar */
-	  //Copiar los elementos restantes de arrIzq[].
-	  while (i < n1) {
-              cm++;
-	    v[k] = arrIzq[i];
-	    i++;
-	    k++;
-	  }
-	  //Copiar los elementos restantes de arrDer[].
-	  while (j < n2) {
-              cm++;
-	    v[k] = arrDer[j];
-	    j++;
-	    k++;
-	  }
-	}
-    
-    @Override
-    public String toString(){
-        String cad = "Datos DESORDENADOS: ";
-        for (int x : valores){
-            cad += x + ", ";
+    public String burbujaSortDescendente(int[] arreglo){
+        pas=0;
+        for(i=1;i<arreglo.length;i++){
+            for(j=0;j<arreglo.length-1;j++){
+                if(arreglo[j]<arreglo[j+1]){
+                    temp=arreglo[j];
+                    arreglo[j]=arreglo[j+1];
+                    arreglo[j+1]=temp;
+                }
+                pas++;
+            }
         }
-        return cad;
+        return mostrarArreglo(arreglo);
+}
+    //Metodo para ShelLSortAscendente
+    public String ShellSortAscendente(int[] arreglo){
+        int salto,i,j,k,auxiliar;
+        salto=arreglo.length/2;
+        while(salto>0){
+            for(i=salto;i<arreglo.length;i++){
+                j=i-salto;
+                while(j>=0){
+                    k=j+salto;
+                    if(arreglo[j]<=arreglo[k]){
+                        j=-1;
+                    }else{
+                        auxiliar=arreglo[j];
+                        arreglo[j]=arreglo[k];
+                        arreglo[k]=auxiliar;
+                        j-=salto;//j=j-salto;
+                    }
+                }
+            }
+            salto=salto/2;
+        }       
+        return mostrarArreglo(arreglo);
     }
     
+    //Método para ShellSortDescendente
+    public String ShellSortDescendente(int[] arreglo){
+        int salto,i,j,k,auxiliar;
+        salto=arreglo.length/2;
+        while(salto>0){
+            for(i=salto;i<arreglo.length;i++){
+                j=i-salto;
+                while(j>=0){
+                    k=j+salto;
+                    if(arreglo[j]>=arreglo[k]){
+                        j=-1;
+                    }else{
+                        auxiliar=arreglo[j];
+                        arreglo[j]=arreglo[k];
+                        arreglo[k]=auxiliar;
+                        j-=salto;//j=j-salto;
+                    }
+                }
+            }
+            salto=salto/2;
+        }
+        return mostrarArreglo(arreglo);
+    }
     
-    
-}
+
+    public String mostrarArreglo(int[] arreglo) {
+        int k;
+        String cadena = "";
+        for (k = 0; k < arreglo.length; k++) {
+            System.out.print("[" + arreglo[k] + "]" + "\n");
+            cadena = cadena + (k + 1) + "----->" + "[" + arreglo[k] + "]" + "\n";
+        }
+        System.out.println("");
+        return cadena;
+    }
+
+}//FIN CLASE
